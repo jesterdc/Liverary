70865718e49016aff6c302af7c935b4c8587ee08
Jester <delacruz.j.bsinfotech@gmail.com>
2024-06-25 13:53:00 +0800

    Organize and finalized the code using OOP and made the page responsive

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,84 +1,145 @@
-const Liverary = [];
-
-function Book(title, author, pages, read, availability){
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-    this.availability = availability;
-    this.info = () => {
-        console.log(`Title: ${title}, By: ${author}, Pages: ${pages}, ${read ? "Finished Reading" : "Not read yet"}, ${availability ? "Available" : "Not Available"}`);
-            }
+class Book {
+    //Book main information
+    constructor(title, author, pages, read, availability){
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+        this.availability = availability;
     }
-            
-function addBookToLibrary(event) {
-    event.preventDefault(); // Prevent form from submitting
 
-    const title = document.getElementById("title").value;
-    const author = document.getElementById("author").value;
-    const pages = document.getElementById("pages").value;
-    const read = document.getElementById("read").checked; // Assuming it's a checkbox
-    const availability = document.getElementById("availability").checked; // Assuming it's a checkbox
-    
-    if((title == null || title == "") || (author == null || author == "") || (pages == null || pages == "")){
-        alert("Please fill in all fields");    
-        return false;
+    //Prints the book information
+    info()
+    {
+        console.log(`Title: ${this.title}, By: ${this.author}, Pages: ${this.pages}, ${this.read? "Finished Reading" : "Not read yet"}, ${this.availability? "Available" : "Not Available"}`);
     }
-    else{
-        const newBook = new Book(title, author, pages, read, availability)
-        Liverary.push(newBook);
-        
-        document.getElementById("title").value = "";
-        document.getElementById("author").value = "";
-        document.getElementById("pages").value = "";
-        document.getElementById("read").checked = false;
-        document.getElementById("availability").checked = false;
+}
 
-        displayBooks()
+class Library {
+    //Library main information
+    constructor() {
+        this.books = [];
+    }
 
+    displayBook(){
+        const booksContainer = document.getElementById("bookList");
+        booksContainer.innerHTML = ""
+
+        this.books.forEach((e, index) => {
+            booksContainer.innerHTML += 
+            `
+            <div class="card">
+                <div></div>
+                <div class="card-body">
+                    <h2 class="card-title">${e.title}</h2>
+                    <div class="card-author">
+                        <h5 class="card-subtitle mb-2 text-muted">by ${e.author}</h5>
+                        <p class="card-text"><b>${e.pages}</b> Pages</p>
+                    </div>
+                    <div class="card-book-status">
+                        <p class="card-text">${e.read? "<span id='finished'>Finished Reading</span>" : "<span id='not-finished'>Not read yet</span>"}</p>
+                    </div>
+                    <div class="card-book-status">
+                        <p class="card-text">${e.availability? "<span id='available'>Available</span>" : "<span id='not-available'>Not Available</span>"}</p>
+                    </div>
+                </div>
+                <div class="card-buttons">
+                    <button id="delete-button" onClick="library.deleteBook(${index}); library.displayBook();">Delete</button>
+                    <button id="update-button" onClick="library.populateFormToUpdate(${index})">Update</button>
+                </div>
+                <div></div>
+            </div>`;
+
+        });
+    }
+
+    //Adds the book to the array collection of books
+    addBook(newBook){
+        this.books.push(newBook);
+    }
+
+    //Deletes the book from the array
+    deleteBook(index){
+        this.books.splice(index, 1);
+    }
+
+    //Updates the book information using its index
+    updateBook(index, updatedBook){
+        this.books[index] = updatedBook;
+    }
+
+    populateFormToUpdate(index){
+        const book = this.books[index];
+
+        document.getElementById("title").value = book.title;
+        document.getElementById("author").value = book.author;
+        document.getElementById("pages").value = book.pages;
+        document.getElementById("read").checked = book.read;
+        document.getElementById("availability").checked = book.availability;
+
+        document.getElementById("book-input").onsubmit = (event) => {
+            event.preventDefault()
+            this.updateBookForm(index, event)
         }
-}
-        
-        // const sampleBook = new Book("1984", "George Orwell", 1500, true, true);
-        // const sampleBook2 = new Book("Game of Thrones", "George R. R. Martin", 1500, true, true);
-        // Liverary.push(sampleBook, sampleBook2);
-    
-function displayBooks(){
-    const booksContainer = document.getElementById("bookList");
-    booksContainer.innerHTML = "";
+    }
 
-    Liverary.forEach((e, index) => {
-        booksContainer.innerHTML += 
-        `
-        <div class="card">
-            <div></div>
-            <div class="card-body">
-                <h2 class="card-title">${e.title}</h2>
-                <div class="card-author">
-                    <h5 class="card-subtitle mb-2 text-muted">by ${e.author}</h5>
-                    <p class="card-text"><b>${e.pages}</b> Pages</p>
-                </div>
-                <div class="card-book-status">
-                    <p class="card-text">${e.read? "<span id='finished'>Finished Reading</span>" : "<span id='not-finished'>Not read yet</span>"}</p>
-                </div>
-                <div class="card-book-status">
-                    <p class="card-text">${e.availability? "<span id='available'>Available</span>" : "<span id='not-available'>Not Available</span>"}</p>
-                </div>
-            </div>
-            <div class="card-buttons">
-                <button id="delete-button" onClick="deleteBook(${index})">Delete</button>
-                <button id="update-button" onClick="updateBook(${index})">Update</button>
-            </div>
-            <div></div>
-        </div>`
-    });
+    updateBookForm(index, event){
+        event.preventDefault();
+
+        const title = document.getElementById("title").value;
+        const author = document.getElementById("author").value;
+        const pages = document.getElementById("pages").value;
+        const read = document.getElementById("read").checked;
+        const availability = document.getElementById("availability").checked;
+
+        if((title == null || title == "") || (author == null || author == "") || (pages == null || pages == "")){
+            alert("Please fill in all fieldss");    
+            return false;
+        }
+        else {
+            const updatedBook = new Book(title, author, pages, read, availability);
+            this.updateBook(index, updatedBook);
+
+            document.getElementById("title").value = "";
+            document.getElementById("author").value = "";
+            document.getElementById("pages").value = "";
+            document.getElementById("read").checked = false;
+            document.getElementById("availability").checked = false;
+
+            //Revert the submit button to the addBook function
+            document.getElementById("book-input").onsubmit = (event) => this.addBookFromForm(event);
+            this.displayBook();
+        }
+    }
+
+    addBookFromForm(event){
+        event.preventDefault();
+
+        const title = document.getElementById("title").value;
+        const author = document.getElementById("author").value;
+        const pages = document.getElementById("pages").value;
+        const read = document.getElementById("read").checked;
+        const availability = document.getElementById("availability").checked;
+
+        if((title == null || title == "") || (author == null || author == "") || (pages == null || pages == "")){
+            alert("Please fill in all fields");    
+            return false;
+        }
+        else {
+            const newBook = new Book(title, author, pages, read, availability);
+            this.addBook(newBook);
+
+            document.getElementById("title").value = "";
+            document.getElementById("author").value = "";
+            document.getElementById("pages").value = "";
+            document.getElementById("read").checked = false;
+            document.getElementById("availability").checked = false;
+
+            this.displayBook();
+        }
+    }
 }
 
-function deleteBook(index){
-    Liverary.splice(index, 1);
-    displayBooks();
-}
+const library = new Library();
 
-function updateBook(index){
-
-}
+document.getElementById("book-input").onsubmit = (event) => library.addBookFromForm(event);
